28bbff332cdfedf9338003c397483f412cf33349
ysugimoto <sugimoto@wnotes.net>
2018-03-10 12:24:02 +0900

    Improve release step

--- _tools/github-release.sh
+++ _tools/github-release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+### Github create release script
+### This script requires 'jq' command, so if you want to use this script, need to install:
+### sudo apt-get install jq or other platforms.
+
 ## Configuration for your project
 
 ### If you run this script as inline code like Jenkins, define as following:
@@ -14,7 +18,7 @@ ASSETS_DIR="./dist"
 ### Determine  project repository
 #REPOSITORY=":owner/:repo"
 
-### If you are using via some CI servcice, you can use following server specific variable.
+### If you are using via some CI service, you can use following server specific variable.
 
 ### In Circle CI:
 REPOSITORY="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
@@ -23,12 +27,12 @@ REPOSITORY="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 #REPOSITORY="${TRAVIS_REPO_SLUG}"
 
 ### Determine release tag name
-TAG=
+#TAG=
 
-### If you are using via some CI servcice, you can use following server specific variable.
+### If you are using via some CI service, you can use following server specific variable.
 
 ### In Circle CI:
-#TAG="${CIRCLE_TAG}"
+TAG="${CIRCLE_TAG}"
 
 ### In Travis CI:
 #TAG="${TRAVIS_TAG}"
@@ -43,14 +47,14 @@ ENDPOINT="https://api.github.com/repos/${REPOSITORY}/releases"
 echo "Creatting new release as version ${TAG}..."
 RELEASE_ID=$(curl -H "${ACCEPT_HEADER}" -H "${TOKEN_HEADER}" -d "{\"tag_name\": \"${TAG}\", \"name\": \"${TAG}\"}" "${ENDPOINT}" | jq .id)
 
-if [ "${RELEASE_ID}" = "" ]; then
+if [ ! "${RELEASE_ID}" ]; then
   echo "Failed to create release. Please check your configuration."
   exit 1
 fi
 
 RELEASE_URL="https://uploads.github.com/repos/${REPOSITORY}/releases/${RELEASE_ID}/assets"
 
-echo "Github release created as ID: ${RELEASE_ID}. https://github.com/${REPOSITORY}/release"
+echo "Github release created as ID: ${RELEASE_ID}. https://github.com/${REPOSITORY}/releases"
 
 if [ "${ASSETS_DIR}" = "" ]; then
   echo "No upload assets, finished."
@@ -58,13 +62,13 @@ if [ "${ASSETS_DIR}" = "" ]; then
 fi
 
 for FILE in ${ASSETS_DIR}/*; do
-  MIME=$(file -b --mime-type "${ASSETS_DIR}/${FILE}")
-  echo "Uploading assets ${ASSETS_DIR}/${FILE} as ${MIME}..."
+  MIME=$(file -b --mime-type "${FILE}")
+  echo "Uploading assets ${FILE} as ${MIME}..."
   curl -v \
     -H "${ACCEPT_HEADER}" \
     -H "${TOKEN_HEADER}" \
     -H "Content-Type: ${MIME}" \
-    --data-binary "@${ASSETS_DIR}/${FILE}" \
+    --data-binary "@${FILE}" \
     "${RELEASE_URL}?name=${FILE}"
 done
 
