fabe972e283309b4e625b537aae363096e756cd5
ysugimoto <sugimoto@wnotes.net>
2018-08-19 15:03:19 +0900

    Install from function script

--- command/install.go
+++ command/install.go
@@ -2,20 +2,77 @@ package command
 
 import (
 	"os"
+	"strings"
 	"sync"
 
+	"go/parser"
+	"go/token"
 	"io/ioutil"
 	"os/exec"
 	"path/filepath"
 
+	"github.com/scylladb/go-set/strset"
 	"github.com/ysugimoto/go-args"
 
 	"github.com/ysugimoto/ginger/config"
 	"github.com/ysugimoto/ginger/logger"
 )
 
-var dependencyPackages = []string{
-	"github.com/aws/aws-lambda-go",
+// findDependencyPackages finds import packages in your lambda functions.
+// walk functions directory recursively, and add to set.
+func findDependencyPackages(root string) ([]*strset.Set, error) {
+	files, err := listFunctionScriptFiles(root)
+	if err != nil {
+		return nil, err
+	}
+
+	// aws-lamda-go is required of default
+	packages := []*strset.Set{
+		strset.New("github.com/aws/aws-lambda-go"),
+		strset.New(),
+		strset.New(),
+		strset.New(),
+	}
+
+	// Parse impopr section and add to set
+	for _, f := range files {
+		t := token.NewFileSet()
+		ast, err := parser.ParseFile(t, f, nil, parser.ImportsOnly)
+		if err != nil {
+			continue
+		}
+		for _, i := range ast.Imports {
+			pkg := strings.Trim(i.Path.Value, `"`)
+			if !strings.Contains(pkg, ".") {
+				continue
+			}
+			level := len(strings.Split(pkg, "/")) - 3
+			if level < 0 {
+				continue
+			}
+			packages[level].Add(pkg)
+		}
+	}
+
+	return packages, nil
+}
+
+// walk function directoryy recursively and list files which has ".go" extension.
+func listFunctionScriptFiles(root string) ([]string, error) {
+	files := []string{}
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if filepath.Ext(path) == ".go" {
+			files = append(files, path)
+		}
+		return nil
+	})
+	return files, err
 }
 
 // Install is the struct for install ginger project dependencies.
@@ -52,18 +109,31 @@ func (i *Install) Run(ctx *args.Context) {
 	tmpDir, _ := ioutil.TempDir("", "ginger-tmp-packages")
 	defer os.RemoveAll(tmpDir)
 
-	var wg sync.WaitGroup
-	for _, pkg := range dependencyPackages {
-		wg.Add(1)
-		i.log.Printf("Installing %s...\n", pkg)
-		go i.installDependencies(pkg, tmpDir, &wg)
+	deps, err := findDependencyPackages(c.FunctionPath)
+	if err != nil {
+		i.log.Errorf("Find dependency error: %s\n", err.Error())
+		return
 	}
-	wg.Wait()
 
+	for _, pkgs := range deps {
+		var wg sync.WaitGroup
+		pkgs.Each(func(item string) bool {
+			if _, err := os.Stat(filepath.Join(c.LibPath, "src", item)); err == nil {
+				return true
+			}
+			wg.Add(1)
+			i.log.Printf("Installing/Resolving %s...\n", item)
+			go i.installDependencies(item, tmpDir, &wg)
+			return true
+		})
+		wg.Wait()
+	}
 	// Recursive copy
 	if err := i.movePackages(tmpDir, c.LibPath); err != nil {
 		i.log.Error(err.Error())
 	}
+
+	i.log.Info("Installed dependencies successfully.")
 }
 
 // installDependencies installs dependencies via "go get".
